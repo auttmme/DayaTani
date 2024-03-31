43f70a4e65aa70a58f82375c7bc5207233f01331
auttmme <ichautm14@gmail.com>
2024-04-01 06:52:09 +0700

    imp: add authorization for CRUD farmer

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,16 @@ import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Layout from "./components/Layout";
 import FarmListPage from "./pages/FarmList";
+import DetailFarmerPage from "./pages/DetailFarmer";
+import AddFarmerPage from "./pages/AddFarmer";
+import EditFarmerPage from "./pages/EditFarmer";
+
+import axios from "axios";
+
+const auth = import.meta.env.VITE_AUTH;
 
 const queryClient = new QueryClient();
+axios.defaults.headers.common["Authorization"] = auth;
 
 function App() {
 	return (
@@ -13,6 +21,9 @@ function App() {
 				<Layout>
 					<Routes>
 						<Route path="/" element={<FarmListPage />} />
+						<Route path="/detail-farmer/:id" element={<DetailFarmerPage />} />
+						<Route path="/add-farmer" element={<AddFarmerPage />} />
+						<Route path="/edit-farmer/:id" element={<EditFarmerPage />} />
 					</Routes>
 				</Layout>
 			</Router>
